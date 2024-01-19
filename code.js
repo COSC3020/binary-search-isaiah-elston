fbb2e393cc3c7845ce113010cddbfe03fb25b0ab
Isaiah Elston <156853632+isaiah-elston@users.noreply.github.com>
2024-01-19 19:40:25 +0000

    Attempt #2

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ Author: Isaiah Elston
 Project: Binary Search
 
 NOTE: I have very little experience with JavaScript, so I am teaching myself as I go. Therefore, I will be including my sources
-      to seemingly trivial features in the beginning of the class...
+      to seemingly trivial tasks in the beginning of the class...
 
 SOURCES:
 
@@ -13,23 +13,22 @@ https://www.geeksforgeeks.org/binary-search/#
 
 For Math.floor()
 https://stackoverflow.com/questions/4228356/how-to-perform-an-integer-division-and-separately-get-the-remainder-in-javascr
-
-For .indexOf():
-https://stackoverflow.com/questions/7346827/how-to-find-the-array-index-with-a-value
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
 */
 
 function binarySearch(list, element) {
-    if (list.length > 0) {
-        var middleIndex = Math.floor((0 + list.length) / 2)
-    } else {return -1}
-    if (middleIndex == list.indexOf(element)) {
-        return list.indexOf(element);
-    } else if (middleIndex > list.indexOf(element)) {
-        binarySearch(list.slice(0, middleIndex), element)
-    } else if (middleIndex < list.indexOf(element)) {
-        binarySearch(list.slice(middleIndex, list.length + 1), element)
-    } else {
-        return -1;
-    }
+    let leftBound = 0;
+    let rightBound = list.length;
+
+    do {
+        let middleIndex = Math.floor(leftBound + ((rightBound - leftBound) / 2));
+        if (list[middleIndex] == element) {
+            return middleIndex;
+        } else if (list[middleIndex] > element) {
+            rightBound = middleIndex--;
+        } else if (list[middleIndex] < element) {
+            leftBound = middleIndex++;
+        } else {
+            return -1;
+        }
+    } while (rightBound >= 1);
 }
